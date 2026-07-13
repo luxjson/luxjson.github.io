0bf69b5347c46cdccbb6d86c3d942add0ffb8386
Lukz <lucaseduarte6@gmail.com>
2026-07-13 19:21:49 -0300

    aaa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sri from 'vite-plugin-sri';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/'
-})
+  plugins: [
+    react(),
+    sri({
+      hashAlgorithm: 'sha384',
+    }),
+  ],
+  base: '/',
+});
