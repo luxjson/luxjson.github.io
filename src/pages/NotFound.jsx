12901bc83980d46a41433c6ba1441d078e2002fb
Lukz <lucaseduarte6@gmail.com>
2026-08-07 21:46:32 -0300

    fix scale and layout issues

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import '../assets/styles/notfound.css';
 export default function NotFound() {
   return (
     <div className="not-found-container">
-      <i className="fa-solid fa-ban"></i>
+      <i className="fa-solid fa-ban" style={{ fontSize: '100px' }}></i>
       <Link to="/" className="back-home-btn">
         Back to home
       </Link>
